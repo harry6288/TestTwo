e4e9022c0ba280ff07e19f55c29a707b623231df
hardeepkaur <hardeepwalia019@gmail.com>
2019-05-27 11:12:34 -0400

    R1 -----> Red

--- Test/TestingTTC.java
+++ Test/TestingTTC.java
@@ -13,10 +13,15 @@ public class TestingTTC {
 	@After
 	public void tearDown() throws Exception {
 	}
-
+//R1 : A one way trip inside Zone 1
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInsideZone() {
+		String[] from1 = new String[] {"Leslie"};
+		String[] to1 = new String[] {"Don Mills"};
+		TTC r = new TTC();
+		Double result = r.calculateTotal(from1, to1);
+		assertEquals(2.50,result,0.0);
+		
 	}
 
 }
--- src/TTC.java
+++ src/TTC.java
@@ -1,4 +1,11 @@
 
 public class TTC {
+	public double calculateTotal(String[] from, String[] to)
+	{
+		String[] from1= new String[] {"Leslie"};
+		String[] to1 = new String[] {"Don Mills"};
+		return 0.0;
+	}
+
 
 }
